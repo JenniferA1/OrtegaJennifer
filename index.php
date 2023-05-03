a26f995eda5209416209672fabf978cfd9f05bb3
JenniferA1 <131908743+JenniferA1@users.noreply.github.com>
2023-05-03 08:50:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ include("marco-right.php");
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">¡Hola! Me complace presentarme, soy Alvarez Ortega <b> Jennifer </b>, soy una estudiante de Ingeniería en tecnología de la información y comunicación con una gran carrera en la que estudio en la Universidad Técnica Luis Vargas torres de esmeraldas. </p>
+                   <p class="eys-parrafo">¡Hola! Me complace presentarme, soy Alvarez Ortega <b> Jennifer </b>, soy una estudiante de Ingeniería en Tecnología de la Información y Comunicación con una gran carrera en la que estudio en la Universidad Técnica Luis Vargas Torres de Esmeraldas. </p>
                    <p class="eys-parrafo">Desde que inicie mi carrera me gusta mucho la programación como va evolucionando a través de los tiempos me interesa mucho el diseño de páginas web.</p>
                    <p class="eys-parrafo">Me interesa mucho en el diseño de paginas por que puedo ser creativa con las cosas que le aplico y cada diseño que se hace es diferente ya que hay compañeros en la que me piden que les ayude en su diseño puedo agregarle mi estilo propio.</p>
                    <p class="eys-parrafo">Además de eso me gusta mucho lo que es grafica y multimedia ya que puedo involúcrame en el funcionamiento de las aplicaciones y sus diferentes programas que existen en el mundo del internet.</p>
@@ -36,8 +36,8 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios lo realicé en la Unidad Educativa Muisne, me gradué de Química durante mis estudios descubrí una amplia variedad de habilidades que me ayudaron en algunas actividades dentro de la carrera.</p>
-                   <p class="eys-parrafo">Además de mi carrera participe en varias actividades de organizaban en mi cantón entre a un curso de enfermería en la cual me gradué con gran éxito y en la cual complementaria con mis estudios que estaba siguiendo en el colegio.</p>
+                   <p class="eys-parrafo">Mis estudios secundarios lo realicé en la Unidad Educativa Muisne, me gradué de Bachiller en Ciencias durante mis estudios descubrí una amplia variedad de habilidades que me ayudaron en algunas actividades dentro de la carrera.</p>
+                   <p class="eys-parrafo">Además en mi carrera participe en varias actividades que organizaban en mi cantón entre a un curso de enfermería en la cual me gradué con gran éxito y en la cual complementaria con mis estudios que estaba siguiendo en el colegio.</p>
 
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
@@ -88,7 +88,7 @@ document.getElementById("elnombre").innerHTML="Jennifer Ortega ";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
- document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='https://congresoutlvte.org/CabezasNicole/'>Nicole Cabezas</a></p><a href='https://congresoutlvte.org/TapiaBarbara/'>Barbara Tapia</a></p> <a href='https://congresoutlvte.org/RamirezAlan/'>Alan Ramirez</a></p><a href='https://congresoutlvte.org/FrancoIsaac/'>Isaac Franco</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='https://congresoutlvte.org/QuinonezBryan/'>Quinonez Bryan</a></p><a href='https://congresoutlvte.org/TapiaBarbara/'>Barbara Tapia</a></p> <a href='https://congresoutlvte.org/ElintonVasquez/'>Elinton Vasquez</a></p><a href='https://congresoutlvte.org/QuinonezIvan/'>Quinonez Ivan</a></p> ";
  </script>
  
 <?php
